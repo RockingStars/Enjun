e6ea67ed43c4932158331dbad0d470135d7e931d
Robert Monden <dev@robertmonden.com>
2018-03-29 12:57:02 +0200

    Fixed things

--- src/com/rockingstar/main/Main.java
+++ src/com/rockingstar/main/Main.java
@@ -2,7 +2,9 @@ package com.rockingstar.main;
 
 import com.rockingstar.engine.controllers.Engine;
 import com.rockingstar.engine.io.models.Util;
-import com.rockingstar.engine.ServerConnection;
+
+import static javafx.application.Application.launch;
+//import com.rockingstar.engine.ServerConnection;
 
 public class Main {
 
