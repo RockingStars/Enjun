e9292172b9b0f9b02c4f551ea7b3d9f69757d5a8
Robert Monden <dev@robertmonden.com>
2018-04-03 12:07:22 +0200

    ! Fixed server errors

--- src/com/rockingstar/engine/ServerConnection.java
+++ src/com/rockingstar/engine/ServerConnection.java
@@ -15,28 +15,33 @@ public class ServerConnection {
     private static ServerConnection uniqueInstance;
 
     private ServerConnection() {
-        String data = "login";
+        String data = "login Robert";
         String logout = "disconnect";
         try {
-            Socket sock = new Socket("localhost", 8000);
+            Socket sock = new Socket("localhost", 7789);
             PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
 
-            out.print(data);
-            //out.print(logout);
+            out.println(data);
 
-            //Thread.sleep(5000000);
-            out.close();
+            Thread.sleep(5000000);
 
             System.out.println("Data sent");
             while (!in.ready()) {
             }
+
             String input = in.readLine();
             System.out.println(input);
+
+            in.close();
+            out.close();
+            sock.close();
 
         }
         catch(IOException e){
             System.out.println("Connection Failed: " + e);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
