21a7a10b915e89798cd5f9c3a2f0b43fdd56b767
Bert de Boer <bertde.boer@hotmail.com>
2018-04-03 11:49:15 +0200

    27-03

--- src/com/rockingstar/main/Main.java
+++ src/com/rockingstar/main/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Enjun");
 
-        new ServerConnection();
+        ServerConnection.getInstance();
     }
 }
