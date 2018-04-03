5a324b71be761f02cc2b1ee9eefb6c11a58ea983
Bert de Boer <bertde.boer@hotmail.com>
2018-04-03 12:04:44 +0200

    connectie met server werkend

--- src/com/rockingstar/engine/ServerConnection.java
+++ src/com/rockingstar/engine/ServerConnection.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 /**
  * Created by Bert de Boer on 3/27/2018.
@@ -17,8 +18,31 @@ public class ServerConnection {
     private ServerConnection() {
         String data = "login";
         String logout = "disconnect";
+
         try {
-            Socket sock = new Socket("localhost", 8000);
+            Socket s = new Socket("localhost", 8000);
+            Scanner in = new Scanner(System.in);
+            System.out.println("Please enter a message");
+            String clientMessage = in.nextLine();
+
+//Make a printwriter and write the message to the socket
+            PrintWriter writer = new PrintWriter(s.getOutputStream());
+            writer.println(clientMessage); // <- println
+            writer.flush();                // <- flush
+
+//StreamReader to read the response from the server
+            InputStreamReader streamReader = new InputStreamReader(s.getInputStream());
+            BufferedReader reader = new BufferedReader(streamReader);
+
+//Get the response message and print it to console
+            String responseMessage = reader.readLine();
+            System.out.println(responseMessage);
+            reader.close();
+            writer.close();                // <- new location for close (*)
+
+
+
+            /*Socket sock = new Socket("localhost", 8000);
             PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
 
@@ -33,7 +57,7 @@ public class ServerConnection {
             }
             String input = in.readLine();
             System.out.println(input);
-
+*/
         }
         catch(IOException e){
             System.out.println("Connection Failed: " + e);
