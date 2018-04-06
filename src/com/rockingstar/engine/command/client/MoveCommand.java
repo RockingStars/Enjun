35d43ebff40087accef08e89d7138f18e342c36a
Robert Monden <dev@robertmonden.com>
2018-04-06 16:15:22 +0200

    Removed quotes from MoveCommandHandler

--- src/com/rockingstar/engine/command/client/MoveCommand.java
+++ src/com/rockingstar/engine/command/client/MoveCommand.java
@@ -14,6 +14,6 @@ public class MoveCommand implements Command {
 
     @Override
     public void execute() {
-        _serverConnection.send(String.format("move \"%s\"", _move));
+        _serverConnection.send(String.format("move %s", _move));
     }
 }
