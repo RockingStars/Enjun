4b0f2c5f7274da27aaad10ae3fa60c63aa86b07b
Marnick Scholten <m.f.scholten@st.hanze.nl>
2018-04-07 13:27:07 +0200

    userlist added

--- src/com/rockingstar/engine/lobby/views/LobbyView.java
+++ src/com/rockingstar/engine/lobby/views/LobbyView.java
@@ -4,28 +4,22 @@ import com.rockingstar.engine.ServerConnection;
 import com.rockingstar.engine.command.client.CommandExecutor;
 import com.rockingstar.engine.command.client.SendChallengeCommand;
 import com.rockingstar.engine.game.Player;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.control.Alert;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
+
 
 
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class LobbyView {
 
@@ -154,9 +148,24 @@ public class LobbyView {
         players.setMinHeight(500);
         players.setAlignment(Pos.CENTER);
         Label onlinePLayer = new Label("List of online players");
+        Button refresh = new Button("Refresh");
         onlinePLayer.setId("otherText");
+        ListIterator iterator = (ListIterator) _playerList.iterator();
+        ToggleGroup usergroup = new ToggleGroup();
+        while (iterator.hasNext()){
+            Player test = (Player) iterator.next();
+            String test2 = test.getUsername();
+            //System.out.println(test2);
+            RadioButton users = new RadioButton(test2);
+            users.setToggleGroup(usergroup);
 
-        rightPane.getChildren().addAll(onlinePLayer, players);
+            players.getChildren().addAll(users);
+
+        }
+
+
+
+        rightPane.getChildren().addAll(onlinePLayer, refresh, players);
         _lobbyPane.setCenter(rightPane);
 
 
@@ -166,9 +175,22 @@ public class LobbyView {
                 gameModeSelected.setId("gameText");
                 rightPane.getChildren().add(gameModeSelected);
                 if (gameMode.getValue() == "Player vs Player"){
+                    System.out.println("Deze wel");
                     rightPane.getChildren().clear();
                     Button challenge = new Button("Challenge");
                     Button localy = new Button("Play offline");
+
+                    usergroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+                        @Override
+                        public void changed(ObservableValue<? extends Toggle> ov, Toggle t, Toggle t1) {
+                            rightPane.getChildren().clear();
+                            RadioButton chk = (RadioButton)t1.getToggleGroup().getSelectedToggle(); // Cast object to radio button
+                            Label selectedUser = new Label("Selected player: "+ chk.getText());
+                            selectedUser.setId("gameText");
+                            rightPane.getChildren().addAll(onlinePLayer, refresh, players,gameModeSelected,selectedUser, challenge, localy);
+
+                        }
+                    });
                     rightPane.getChildren().addAll(onlinePLayer, players,gameModeSelected, challenge, localy);
                 } else if(gameMode.getValue() == "Player vs AI"){
                     rightPane.getChildren().clear();
@@ -183,6 +205,9 @@ public class LobbyView {
                     test.setId("gameText");
                     rightPane.getChildren().addAll(gameModeSelected, test);
                 }
+
+
+
             } else {
                 Alert noGameModeSelected = new Alert(Alert.AlertType.INFORMATION);
                 noGameModeSelected.setTitle("No game mode selecterd");
@@ -190,12 +215,31 @@ public class LobbyView {
                 noGameModeSelected.setContentText("ERMAHGERD!!! You did not select a game mode, please select a game mode to continue");
                 noGameModeSelected.showAndWait();
             }
+
+
         });
+
+
+
+
+        refresh.setOnAction(event -> {
+            players.getChildren().clear();
+            ListIterator iterator2 = (ListIterator) _playerList.iterator();
+            while (iterator2.hasNext()){
+                Player test = (Player) iterator2.next();
+                String test2 = test.getUsername();
+                System.out.println(test2);
+                RadioButton users = new RadioButton(test2);
+                players.getChildren().add(users);
+            }
+
+        });
+
+
 
         return _lobbyPane;
 
     }
-
     public void setPlayerList(LinkedList<Player> playerList) {
         _playerList = playerList;
     }
