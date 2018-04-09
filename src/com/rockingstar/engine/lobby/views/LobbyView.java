f10b2a9defa7546d57e272e61d85e988f6ed2644
Marnick Scholten <m.f.scholten@st.hanze.nl>
2018-04-09 20:40:18 +0200

    Scrollpane functioning, cant play yourself

--- src/com/rockingstar/engine/lobby/views/LobbyView.java
+++ src/com/rockingstar/engine/lobby/views/LobbyView.java
@@ -3,7 +3,11 @@ package com.rockingstar.engine.lobby.views;
 import com.rockingstar.engine.ServerConnection;
 import com.rockingstar.engine.command.client.CommandExecutor;
 import com.rockingstar.engine.command.client.SendChallengeCommand;
+import com.rockingstar.engine.game.AbstractGame;
+import com.rockingstar.engine.game.GameInterface;
 import com.rockingstar.engine.game.Player;
+import com.rockingstar.engine.lobby.controllers.Launcher;
+import com.rockingstar.engine.lobby.models.LobbyModel;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
@@ -43,6 +47,8 @@ public class LobbyView {
 
     private String _username;
 
+    RadioButton users;
+
     private Button _buttonGame0;
     private Button _buttonGame1;
     private Button _buttonGame2;
@@ -66,10 +72,8 @@ public class LobbyView {
         topPane.setAlignment(Pos.CENTER);
 
         Label titelLable =  new Label("Welcome " + _username);
-        //Label selectGame = new Label("Please select your game");
 
         titelLable.setId("topText");
-        //selectGame.setId("otherText");
 
         topPane.getChildren().addAll(titelLable);
         _lobbyPane.setTop(topPane);
@@ -81,7 +85,7 @@ public class LobbyView {
 
         VBox menu = new VBox(20);
         menu.setStyle("-fx-border-color: red");
-        menu.setSpacing(60);
+        menu.setSpacing(30);
         menu.setMaxWidth(width/4);
         menu.setMinHeight(800);
         menu.setAlignment(Pos.CENTER);
@@ -92,7 +96,7 @@ public class LobbyView {
 
         gameSelectionBox.getItems().addAll("Reversi", "TicTacToe", "etc" );
         Label gameModeText = new Label("How do you want to play?");
-        Button selectGame = new Button("continue");
+        Button selectGame = new Button("Select this game");
         gameModeText.setId("gameText");
         ComboBox gameMode = new ComboBox();
         gameMode.getItems().addAll("Player vs Player", "Player vs AI", "AI vs AI");
@@ -102,10 +106,10 @@ public class LobbyView {
 
 
         selectGame.setOnAction(e -> {
-            System.out.println("test");
             if (gameSelectionBox != null){
                 menu.getChildren().clear();
-
+                Label gameSwitch = new Label("Do you want to switch to another game?");
+                gameSwitch.setId("gameText");
                 Button gameImage = new Button("");
                 Label gameName = new Label("You have selected " + gameSelectionBox.getValue());
                 gameName.setId("gameText");
@@ -120,28 +124,20 @@ public class LobbyView {
                     gameImage.setId("gameImage2");
                 }
 
-
-                menu.getChildren().addAll(gameSelectionBox,selectGame, gameName,gameModeText ,gameMode, gameImage, goButton);
+                menu.getChildren().addAll(gameName, gameSwitch, gameSelectionBox,selectGame,gameModeText ,gameMode, gameImage, goButton);
             }
         });
-
-
-
-
-
-
-//        gameImage.getChildren().addAll(imageLabel);
-//
 
 
         menu.getChildren().addAll(gameSelectionText, gameSelectionBox, selectGame);
         leftPane.getChildren().addAll(menu);
         _lobbyPane.setLeft(leftPane);
 
+        //Right
         ScrollPane scrollPane = new ScrollPane();
         scrollPane.setMaxWidth(width/5);
-        scrollPane.setMinHeight(500);
-
+        scrollPane.setMaxHeight(500);
+        scrollPane.setId("scrollPane");
 
         VBox rightPane = new VBox(20);
         rightPane.setStyle("-fx-border-color: red");
@@ -151,9 +147,6 @@ public class LobbyView {
 
 
         VBox players = new VBox(20);
-        players.setStyle("-fx-border-color: blue");
-//        players.setMaxWidth(width/5);
-//        players.setMinHeight(500);
         players.setAlignment(Pos.CENTER);
         Label onlinePLayer = new Label("List of online players");
         Button refresh = new Button("Refresh");
@@ -161,30 +154,23 @@ public class LobbyView {
         ListIterator iterator = (ListIterator) _playerList.iterator();
         ToggleGroup usergroup = new ToggleGroup();
 
-        while (iterator.hasNext()){
-            Player test = (Player) iterator.next();
-            String test2 = test.getUsername();
-            //System.out.println(test2);
-            RadioButton users = new RadioButton(test2);
-            users.setToggleGroup(usergroup);
+        while (iterator.hasNext()) {
 
+            Player nextPlayer = (Player) iterator.next();
+            String usernameString = nextPlayer.getUsername();
+            if (usernameString.equals(_username)){
+                users = new RadioButton(usernameString + " (me)");
+            } else {
+                users = new RadioButton(usernameString);
+            }
+            users.setToggleGroup(usergroup);
+            users.translateXProperty().bind(scrollPane.widthProperty().subtract(users.widthProperty()).divide(2));
+            users.setId("users");
             players.getChildren().addAll(users);
             scrollPane.setContent(players);
         }
 
-
-        //scrollPane.setContent(players);
         rightPane.getChildren().addAll(onlinePLayer, refresh, scrollPane);
-
-
-        //rightPane.getChildren().addAll(rightPane, scrollPane);
-//        sc.valueProperty().addListener(new ChangeListener<Number>() {
-//            @Override
-//            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-//                rightPane.setLayoutX(-newValue.doubleValue());
-//            }
-//        });
-
 
         _lobbyPane.setCenter(rightPane);
 
@@ -195,7 +181,6 @@ public class LobbyView {
                 gameModeSelected.setId("gameText");
                 rightPane.getChildren().add(gameModeSelected);
                 if (gameMode.getValue() == "Player vs Player"){
-                    System.out.println("Deze wel");
                     rightPane.getChildren().clear();
                     Button challenge = new Button("Challenge");
                     Button locally = new Button("Play offline");
@@ -205,13 +190,29 @@ public class LobbyView {
                         public void changed(ObservableValue<? extends Toggle> ov, Toggle t, Toggle t1) {
                             rightPane.getChildren().clear();
                             RadioButton chk = (RadioButton)t1.getToggleGroup().getSelectedToggle(); // Cast object to radio button
-                            Label selectedUser = new Label("Selected player: "+ chk.getText());
+                            Label selectedUser = new Label("Selected player: " + chk.getText());
                             selectedUser.setId("gameText");
-                            rightPane.getChildren().addAll(onlinePLayer, refresh, players,gameModeSelected,selectedUser, challenge, locally);
+                            if (chk.getText().equals(_username + " (me)")){
+                                challenge.setOnAction(event -> {
+                                    Alert challengeMe = new Alert(Alert.AlertType.INFORMATION);
+                                    challengeMe.setTitle("You cannot challenge yourself");
+                                    challengeMe.setHeaderText(null);
+                                    challengeMe.setContentText("You can not challenge yourself!, please challenge another user");
+                                    challengeMe.showAndWait();
+                                });
+                            } else {
+                                challenge.setOnAction(event -> {
+                                    CommandExecutor.execute(new SendChallengeCommand(ServerConnection.getInstance(), new Player(chk.getText()), "Reversi"));
+                                });
+                        }
+
+                                rightPane.getChildren().addAll(onlinePLayer, refresh, scrollPane, gameModeSelected, selectedUser, challenge, locally);
 
                         }
                     });
-                    rightPane.getChildren().addAll(onlinePLayer, players,gameModeSelected, challenge, locally);
+
+                    rightPane.getChildren().addAll(onlinePLayer, scrollPane,gameModeSelected, challenge, locally);
+
                 } else if(gameMode.getValue() == "Player vs AI"){
                     rightPane.getChildren().clear();
                     Label difficulty = new Label("Select your difficulty");
@@ -227,7 +228,6 @@ public class LobbyView {
                 }
 
 
-
             } else {
                 Alert noGameModeSelected = new Alert(Alert.AlertType.INFORMATION);
                 noGameModeSelected.setTitle("No game mode selecterd");
@@ -240,22 +240,19 @@ public class LobbyView {
         });
 
 
-
-
         refresh.setOnAction(event -> {
-            players.getChildren().clear();
+            scrollPane.setContent(null);
             ListIterator iterator2 = (ListIterator) _playerList.iterator();
             while (iterator2.hasNext()){
                 Player test = (Player) iterator2.next();
                 String test2 = test.getUsername();
                 System.out.println(test2);
                 RadioButton users = new RadioButton(test2);
-                players.getChildren().add(users);
+                System.out.println("set content:");
+                scrollPane.setContent(users);
             }
 
         });
-
-
 
         return _lobbyPane;
 
