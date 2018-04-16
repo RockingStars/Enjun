b6687296bde089fd95df6642dd35611a50e2bcb7
Marnick Scholten <m.f.scholten@st.hanze.nl>
2018-04-16 18:55:13 +0200

    Timer class added, reversi Ingame music added and style changes

---
+++ src/com/rockingstar/engine/game/Timer.java
@@ -0,0 +1,33 @@
+package com.rockingstar.engine.game;
+
+import javafx.application.Platform;
+import javafx.scene.control.Label;
+
+public class Timer extends Thread {
+
+    private Label _countLabel;
+
+    public Timer(Label countLabel) {
+        _countLabel = countLabel;
+    }
+
+    @Override
+    public void run(){
+        int counter = 10;
+
+        while (counter >= 0 && !interrupted()) {
+            final int finalCounter = counter;
+
+            Platform.runLater(() -> _countLabel.setText("" + finalCounter));
+            System.out.println("Hierzo: " + counter);
+            counter = counter - 1;
+
+            try {
+                Thread.sleep(1000);
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
